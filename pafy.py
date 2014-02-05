244a83b48160a2bdd6e38f3f1c0cba9fc78d444f
darren <np1nagev@gmail.com>
2014-02-05 17:06:20 +0000

    bump version numbers

--- pafy.py
+++ pafy.py
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
-__version__ = "0.3.31"
+__version__ = "0.3.32"
 __author__ = "nagev"
 __license__ = "GPLv3"
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(
     name='Pafy',
     py_modules=['pafy'],
-    version='0.3.31',
+    version='0.3.32',
     description="Python API for YouTube, query and download YouTube content",
     keywords=["Pafy", "API", "YouTube", "youtube", "download", "video"],
     author="nagev",
