20f999bc43318d59b097335744e413014a8ede79
darren <np1nagev@gmail.com>
2014-05-17 13:23:05 +0100

    Modified classifiers

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+""" setup.py for pafy.
+
+https://np1.github.com/np1/pafy
+
+python setup.py sdist bdist_wheel
+
+"""
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='Pafy',
-    py_modules=['pafy'],
+    packages=['pafy'],
+    scripts=['scripts/ytdl'],
     version='0.3.43',
     description="Python API for YouTube, query and download YouTube content",
     keywords=["Pafy", "API", "YouTube", "youtube", "download", "video"],
@@ -13,23 +26,28 @@ setup(
     author_email="np1nagev@gmail.com",
     url="http://np1.github.io/pafy/",
     download_url="https://github.com/np1/pafy/tarball/master",
-    scripts=['ytdl'],
     package_data={"": ["LICENSE", "README.rst", "CHANGELOG"]},
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS 9",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows XP",
+        "Operating System :: Microsoft :: Windows :: Windows Vista",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.0",
-        "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Development Status :: 5 - Production/Stable",
         "Topic :: Multimedia :: Sound/Audio :: Capture/Recording",
         "Topic :: Utilities",
         "Topic :: Multimedia :: Video",
         "Topic :: Internet :: WWW/HTTP"],
-    long_description=open("README").read()
+    long_description=open("README.rst").read()
 )
