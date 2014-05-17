5710ddaf2558005beb418518c089c858fc878468
darren <np1nagev@gmail.com>
2014-05-17 13:27:10 +0100

    remove api from namespace

---
+++ pafy/__init__.py
@@ -0,0 +1,14 @@
+from pafy.api import get_playlist
+from pafy.api import new
+import sys
+
+__version__ = "0.3.43"
+__author__ = "nagev"
+__license__ = "GPLv3"
+
+if not "test" in sys.argv[0]:
+    del api
+
+del sys
+
+__all__ = "get_playlist new".split()
