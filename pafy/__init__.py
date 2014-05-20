40bd2291bb247dbd369e3f2e4fcc7905b6beaec6
darren <np1nagev@gmail.com>
2014-05-20 17:28:41 +0100

    remove package data from __init__

--- pafy/__init__.py
+++ pafy/__init__.py
@@ -1,14 +1,11 @@
 from .pafy import get_playlist
 from .pafy import new
+from .pafy import __version__
+from .pafy import __author__
+from .pafy import __license__
 import sys
-
-__version__ = "0.3.43"
-__author__ = "nagev"
-__license__ = "GPLv3"
 
 if "test" not in sys.argv[0]:
     del pafy
 
 del sys
-
-__all__ = "get_playlist new".split()
