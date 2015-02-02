8b90fabc629156d377c7b4edc145cd076d0b329d
np1 <np1nagev@gmail.com>
2015-02-03 00:28:18 +1100

    Update version to 0.3.72

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name='Pafy',
     packages=['pafy'],
     scripts=['scripts/ytdl'],
-    version='0.3.70',
+    version='0.3.72',
     description="Retrieve YouTube content and metadata",
     keywords=["Pafy", "API", "YouTube", "youtube", "download", "video"],
     author="np1",
