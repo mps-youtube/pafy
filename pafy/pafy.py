f96712526b0e6ed580e501f14623fca687d4d2c0
Ian D. Scott <ian@perebruin.com>
2015-05-16 12:52:52 -0700

    Fix ImportError in py2

--- pafy/pafy.py
+++ pafy/pafy.py
@@ -40,7 +40,6 @@ import logging
 import hashlib
 import tempfile
 from xml.etree import ElementTree
-from urllib.parse import urlencode
 
 
 early_py_version = sys.version_info[:2] < (2, 7)
@@ -49,12 +48,12 @@ if sys.version_info[:2] >= (3, 0):
     # pylint: disable=E0611,F0401,I0011
     from urllib.request import build_opener
     from urllib.error import HTTPError, URLError
-    from urllib.parse import parse_qs, unquote_plus
+    from urllib.parse import parse_qs, unquote_plus, urlencode
     uni, pyver = str, 3
 
 else:
     from urllib2 import build_opener, HTTPError, URLError
-    from urllib import unquote_plus
+    from urllib import unquote_plus, urlencode
     from urlparse import parse_qs
     uni, pyver = unicode, 2
 
