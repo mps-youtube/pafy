098de05b4238b53a93628aabc92434dafff1cb9f
Ian D. Scott <ian@perebruin.com>
2015-07-25 14:24:31 -0700

    Fix video id extracter without scheme

--- pafy/pafy.py
+++ pafy/pafy.py
@@ -190,6 +190,8 @@ def extract_video_id(url):
     if idregx.match(url):
         return url # ID of video
 
+    if '://' not in url:
+        url = '//' + url
     parsedurl = urlparse(url)
     if "youtube.com" in parsedurl.netloc:
         query = parse_qs(parsedurl.query)
