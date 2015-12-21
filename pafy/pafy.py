df93bd9a8cc26a73bb422beb60c0ea4dde1b1f80
Ian D. Scott <ian@perebruin.com>
2015-12-20 17:48:25 -0800

    Version 0.3.80

--- pafy/pafy.py
+++ pafy/pafy.py
@@ -26,7 +26,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 
-__version__ = "0.3.78"
+__version__ = "0.3.80"
 __author__ = "np1"
 __license__ = "LGPLv3"
 
