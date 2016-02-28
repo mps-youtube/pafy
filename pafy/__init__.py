d1207ee996815cd2b84cf825c0f3eddec2134956
Ian D. Scott <ian@iandouglasscott.com>
2016-02-28 12:22:11 -0800

    Allow accessing backend name

--- pafy/__init__.py
+++ pafy/__init__.py
@@ -10,6 +10,7 @@ from .pafy import new
 from .pafy import set_api_key
 from .pafy import load_cache, dump_cache
 from .pafy import get_categoryname
+from .pafy import backend
 from .util import GdataError, call_gdata
 from .playlist import get_playlist, get_playlist2
 
