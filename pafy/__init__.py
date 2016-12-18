9f32ac7ccb43cba59b5469a7d1c294f92e170ae4
Ian Douglas Scott <ian@iandouglasscott.com>
2016-12-18 12:23:28 -0800

    Remove "del pafy"
    This isn't really necessary

--- pafy/__init__.py
+++ pafy/__init__.py
@@ -1,5 +1,3 @@
-import sys
-
 __version__ = "0.5.2"
 __author__ = "np1"
 __license__ = "LGPLv3"
@@ -13,8 +11,3 @@ from .pafy import get_categoryname
 from .pafy import backend
 from .util import GdataError, call_gdata
 from .playlist import get_playlist, get_playlist2
-
-if "test" not in sys.argv[0]:
-    del pafy
-
-del sys
