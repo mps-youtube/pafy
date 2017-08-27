047daab39c9e47452a45fa15916e9a309bb0cb5e
Thomas Schneider <maxmusterm@gmail.com>
2017-08-27 22:28:02 +0200

    added video statistic pull

--- pafy/channel.py
+++ pafy/channel.py
@@ -23,7 +23,7 @@ def extract_channel(channel_url):
             channel_id=allinfo['items'][0]['id']
     return channel_id
 
-def get_channel(channel_url, basic=False, gdata=False,
+def get_channel_videos(channel_url, basic=False, gdata=False,
                  size=False, callback=lambda x: None):
     if not channel_url:
         err = "Channel does not exist: %s"
@@ -31,23 +31,36 @@ def get_channel(channel_url, basic=False, gdata=False,
 
     channel_id=extract_channel(channel_url)
 
-    video_links = []
+    video_ids = []
     query = {'part':'snippet,id',
             'channelId':channel_id, 'order':'date','maxResults':25}
     while True:
         resp = call_gdata('search', query)
-
         for i in resp['items']:
             if i['id']['kind'] == "youtube#video":
-                video_links.append(i['id']['videoId'])
-
+                video_ids.append(i['id']['videoId'])
         try:
             next_page_token = resp['nextPageToken']
             query['pageToken']=next_page_token
         except:
             break
-    return video_links
+    # get statistics in chunks of size 50
+    for i in range(0, len(video_ids), 50):
+        videoid_chunk=video_ids[i:i+50]
+        query = {'part':'statistics', 'id':','.join(videoid_chunk)}
+        vid_rsp = call_gdata('videos', query)
+        print(vid_rsp)
+
+    return vid_rsp
+
 
 class Channel:
     def __init__(self, channel_url, gdata):
         pass
+
+a="https://www.youtube.com/user/EEVblog"
+b="https://www.youtube.com/channel/UClTpDNIOtgfRkyT-AFGNWVw"
+c="https://www.youtube.com/user/BlackLotus89chan"
+d="https://www.youtube.com/channel/UCkGvUEt8iQLmq3aJIMjT2qQ"
+#print(get_channel_videos(a))
+print(get_channel_videos(a))
