97d926836d36cbcba1abd8be6e103a1f9ed2e944
Vivo Jay <67545205+Vivojay@users.noreply.github.com>
2021-12-18 18:00:31 +0530

    Update backend_youtube_dl.py

--- pafy/backend_youtube_dl.py
+++ pafy/backend_youtube_dl.py
@@ -50,7 +50,13 @@ class YtdlPafy(BasePafy):
         self._length = self._ydl_info['duration']
         self._viewcount = self._ydl_info['view_count']
         self._likes = self._ydl_info.get('like_count', 0)
-        self._dislikes = self._ydl_info.get('dislike_count', 0)
+
+        # As of November 2021, YT did away with dislike counts
+        try:
+            self._dislikes = self._ydl_info['dislike_count']
+        except KeyError:
+            self._dislikes = '[YT_REDACTED]'
+
         self._username = self._ydl_info['uploader_id']
         self._category = self._ydl_info['categories'][0] if self._ydl_info['categories'] else ''
         self._bestthumb = self._ydl_info['thumbnails'][0]['url']
