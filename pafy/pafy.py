3710fb6ab6d785da5424bd2b81875a494a6e4d06
Jonathan <jtorres_1@outlook.com>
2023-05-22 23:35:46 -0600

    Added changes from https://tinyurl.com/29e5auue

--- pafy/pafy.py
+++ pafy/pafy.py
@@ -45,7 +45,7 @@ Pafy = None
 backend = "internal"
 if os.environ.get("PAFY_BACKEND") != "internal":
     try:
-        import youtube_dl
+        import yt_dlp as youtube_dl
         backend = "youtube-dl"
     except ImportError:
         raise ImportError(
